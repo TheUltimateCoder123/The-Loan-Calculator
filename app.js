69460a4bb95bb4ec874e9aed9f3b40cffdd94780
Hemant Devkota <hemantdevkota100billion@gmail.com>
2021-08-21 12:51:15 +0530

    calculor maths added

---
+++ app.js
@@ -0,0 +1,47 @@
+document.querySelector('#btn').addEventListener('click',function(e){
+
+
+
+
+const fetchAmount=document.querySelector(".amount");
+const fetchRate=document.querySelector(".interest");
+const fetchYears=document.querySelector(".years");
+
+const monthlyPayment=document.querySelector(".monthPay");
+const totalInterest=document.querySelector(".totalInt");
+const totalPayments=document.querySelector(".totalPay");
+
+
+const principal=parseFloat(fetchAmount.value);
+const calculatedInterest=parseFloat(fetchRate.value)/100/12;
+const calculatedPayments=parseFloat(fetchYears.value)*12;
+
+
+//Formula for Monthly Payements
+const x=Math.pow(1+calculatedInterest,calculatedPayments);
+const monthly=(principal*x*calculatedInterest)/(x-1);
+
+
+if(isFinite(monthly)){
+monthlyPayment.textContent=monthly.toFixed(2);
+totalPayments.textContent=(monthly*calculatedPayments).toFixed(2);
+totalInterest.textContent=((monthly*calculatedPayments)-principal).toFixed(2);
+
+
+}
+else{
+    console.log("enter values correctly")
+}
+
+
+
+
+
+
+
+
+console.log(fetchAmount);
+console.log(fetchRate);
+console.log(fetchYears);
+    e.preventDefault();
+})
